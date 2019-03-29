885a462d4ea579c28e5027d7a65163fda6507871
unknown <piyushpadhy4444@gmail.com>
2019-03-29 16:22:35 +0530

    First commit

---
+++ server/utils/users.js
@@ -0,0 +1,62 @@
+[{
+  id: '/#12poiajdspfoif',
+  name: 'Andrew',
+  room: 'The Office Fans'
+}]
+
+// addUser(id, name, room)
+// removeUser(id)
+// getUser(id)
+// getUserList(room)
+
+class Users {
+  constructor () {
+    this.users = [];
+  }
+  addUser (id, name, room) {
+    var user = {id, name, room};
+    this.users.push(user);
+    return user;
+  }
+  removeUser (id) {
+    var user = this.getUser(id);
+
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id);
+    }
+
+    return user;
+  }
+  getUser (id) {
+    return this.users.filter((user) => user.id === id)[0]
+  }
+  getUserList (room) {
+    var users = this.users.filter((user) => user.room === room);
+    // console.log(users);
+    var namesArray = users.map((user) => {
+      let result={name:user.name,
+      id:user.id}
+      return result;
+
+    });
+    // console.log(namesArray)
+
+    return namesArray;
+  }
+}
+
+module.exports = {Users};
+
+ // class Person {
+ //   constructor (name, age) {
+ //     this.name = name;
+ //     this.age = age;
+ //   }
+ //   getUserDescription () {
+ //     return `${this.name} is ${this.age} year(s) old.`;
+ //   }
+ // }
+ //
+ // var me = new Person('Andrew', 25);
+ // var description = me.getUserDescription();
+ // console.log(description);
